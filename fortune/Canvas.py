dfdf6ab81fc2f2dd3151c60f5527f2026eda5568
Ahmad Hanif Adisetya <ahmad.hanif11@ui.ac.id>
2024-11-18 23:16:52 +0700

    refactor

---
+++ fortune/Canvas.py
@@ -0,0 +1,62 @@
+from PyQt5.QtWidgets import QGraphicsScene
+from PyQt5.QtGui import QPen
+from PyQt5.QtCore import Qt, QPointF
+from Voronoi import VoronoiDiagram
+
+
+class Canvas(QGraphicsScene):
+    def __init__(self):
+        super().__init__()
+        self.points = []
+        self.circles = []
+        self.setSceneRect(0, 0, 1300, 1000)
+
+    # Add a point
+    def addPoint(self, point):
+        self.addEllipse(point.x()-5, point.y()-5, 10, 10, QPen(Qt.black), Qt.black)
+        self.points.append((point.x(), point.y()))
+
+    # Draw edges of voronoi 
+    def drawEdges(self, edges):
+        for edge in edges:
+            self.addLine(edge[0], edge[1], edge[2], edge[3], QPen(Qt.red))
+
+    # Clear the canvas
+    def clearCanvas(self):
+        self.clear()
+        self.points = []
+
+    # Load points from file
+    def loadPoints(self, file):
+        for line in file:
+            x, y = line.strip().split(',')
+            x, y = float(x), 1100-float(y)
+            self.addPoint(QPointF(x, y))
+    
+    # Draw largest empty circle
+    def addCircle(self):
+        if not self.circles:
+            return
+
+        largest_circle = max(self.circles, key=lambda c: c[2])
+        cx, cy, radius = largest_circle
+
+        pen = QPen(Qt.blue)
+        self.addEllipse(
+            cx - radius, cy - radius,
+            2 * radius, 2 * radius,
+            pen
+        )
+
+    # Calculate voronoi
+    def calculate(self):
+        if self.points:
+            self.clear()
+            for x, y in self.points:
+                self.addEllipse(x-5, y-5, 10, 10, QPen(Qt.black), Qt.black)
+            voronoi = VoronoiDiagram(self.points)
+            voronoi.process()
+            temp = voronoi.get_output()
+            edges = temp[0]
+            self.circles = temp[1] 
+            self.drawEdges(edges)
--- fortune/main.py
+++ fortune/main.py
@@ -1,7 +1,7 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QHBoxLayout, QWidget, QGraphicsScene, QGraphicsView, QFileDialog
-from PyQt5.QtGui import QPainter, QPen
-from PyQt5.QtCore import Qt, QPointF
-from Voronoi import VoronoiDiagram
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QGraphicsScene, QGraphicsView, QFileDialog
+from PyQt5.QtGui import QPainter
+from PyQt5.QtCore import Qt
+from Canvas import Canvas
 
 
 class MainWindow(QMainWindow):
@@ -11,101 +11,59 @@ class MainWindow(QMainWindow):
         self.circles = []
         self.showFullScreen()
 
-        # Main layout
-        main_widget = QWidget()
-        main_layout = QHBoxLayout()
-        main_widget.setLayout(main_layout)
-        self.setCentralWidget(main_widget)
+        # Setup GUI
+        widget = QWidget()
+        layout = QHBoxLayout()
+        layoutBtn = QVBoxLayout()
+        layoutBtn.setAlignment(Qt.AlignVCenter)
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
 
-        # Canvas setup
-        self.scene = QGraphicsScene()
-        self.view = QGraphicsView(self.scene)
+        self.canvas = Canvas()
+        self.view = QGraphicsView(self.canvas)
         self.view.setRenderHint(QPainter.Antialiasing)
         self.view.setFixedSize(1400, 1100)
-        self.scene.setSceneRect(0, 0, 1300, 1000) 
-        main_layout.addWidget(self.view)
-
-        # Buttons
-        self.btnCalculate = QPushButton("Calculate Voronoi")
-        self.btnCalculate.clicked.connect(self.calculate)
-        main_layout.addWidget(self.btnCalculate)
-
-        self.btnLargestCircle = QPushButton("Largest Circle")
-        self.btnLargestCircle.clicked.connect(self.largestCircle)
-        main_layout.addWidget(self.btnLargestCircle)
-
-        self.btnLoad = QPushButton("Load Points")
-        self.btnLoad.clicked.connect(self.loadPoints)
-        main_layout.addWidget(self.btnLoad)
-
-        self.btnClear = QPushButton("Clear Canvas")
-        self.btnClear.clicked.connect(self.clear)
-        main_layout.addWidget(self.btnClear)
-
         self.view.setMouseTracking(False)
         self.view.viewport().installEventFilter(self)
+        self.canvas.setSceneRect(0, 0, 1300, 1000) 
+        layout.addWidget(self.view)
 
+        # Setup Buttons
+        self.calculateBtn = QPushButton("Calculate Voronoi")
+        self.calculateBtn.clicked.connect(self.canvas.calculate)
+        self.largestCircleBtn = QPushButton("Largest Circle")
+        self.largestCircleBtn.clicked.connect(self.canvas.addCircle)
+        self.fileBtn = QPushButton("Load Points")
+        self.fileBtn.clicked.connect(self.loadPoints)
+        self.clearBtn = QPushButton("Clear Canvas")
+        self.clearBtn.clicked.connect(self.canvas.clearCanvas)
+        layoutBtn.addWidget(self.calculateBtn)
+        layoutBtn.addWidget(self.largestCircleBtn)
+        layoutBtn.addWidget(self.fileBtn)
+        layoutBtn.addWidget(self.clearBtn)
+        layout.addLayout(layoutBtn)
+
+    # Catch mouse button click
     def eventFilter(self, source, event):
         if event.type() == event.MouseButtonPress:
             pos = event.pos()
-            scene_pos = self.view.mapToScene(pos)
-            self.addPoint(scene_pos)
+            p = self.view.mapToScene(pos)
+            self.canvas.addPoint(p)
             return True
         return super().eventFilter(source, event)
-    
+
+    # Load points from files
     def loadPoints(self):
         filename, _ = QFileDialog.getOpenFileName(self, "Open Points File", "", "Text Files (*.txt)")
         if filename:
             with open(filename, 'r') as file:
-                for line in file:
-                    x, y = line.strip().split(',')
-                    x, y = float(x), 1100-float(y)
-                    self.addPoint(QPointF(x, y))
-
-    def addPoint(self, point):
-        self.scene.addEllipse(point.x()-5, point.y()-5, 10, 10, QPen(Qt.black), Qt.black)
-        self.points.append((point.x(), point.y()))
-
-    def calculate(self):
-        self.scene.clear()
-        for x, y in self.points:
-            self.scene.addEllipse(x-5, y-5, 10, 10, QPen(Qt.black), Qt.black)
-        voronoi = VoronoiDiagram(self.points)
-        voronoi.process()
-        temp = voronoi.get_output()
-        edges = temp[0]
-        self.circles = temp[1] 
-        self.drawEdges(edges)
-
-    def largestCircle(self):
-        if not self.circles:
-            return
-
-        largest_circle = max(self.circles, key=lambda c: c[2])
-        cx, cy, radius = largest_circle
-
-        pen = QPen(Qt.blue)
-        self.scene.addEllipse(
-            cx - radius, cy - radius,
-            2 * radius, 2 * radius,
-            pen
-        )
-
-    def clear(self):
-        self.scene.clear()
-        self.points = []
-
-    def drawEdges(self, edges):
-        for edge in edges:
-            self.scene.addLine(edge[0], edge[1], edge[2], edge[3], QPen(Qt.red))
-
+                self.canvas.loadPoints(file)
 
 def main():
     app = QApplication([])
     window = MainWindow()
     window.show()
     app.exec_()
-
 
 if __name__ == '__main__':
     main()
